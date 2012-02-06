1becb8888932e1c3726637debe5e331a7711f2ea
Arco Mul <me@arcomul.nl>
2012-02-06 14:41:45 +0100

    Character can only walk one direction at a time 	modified:   ../DangerousGame/MainCharacter.cs

--- DangerousGame/DangerousGame/MainCharacter.cs
+++ DangerousGame/DangerousGame/MainCharacter.cs
@@ -12,7 +12,6 @@ namespace DangerousGame
 {
     class MainCharacter : Sprite
     {
-
         private int Speed = 120;
         private Vector2 Direction = Vector2.Zero;
         private Vector2 Velocity = Vector2.Zero;
@@ -24,7 +23,8 @@ namespace DangerousGame
 
         private Map Map;
 
-        KeyboardState PreviousKeyboardState;
+        private List<Keys> pressedKeys = new List<Keys>();
+        private Keys lastPressedKey;
 
         public MainCharacter (Map map)
         {
@@ -35,7 +35,6 @@ namespace DangerousGame
         {
             KeyboardState KeyboardState = Keyboard.GetState();
             UpdateMovement(KeyboardState, gameTime);
-            PreviousKeyboardState = KeyboardState;
             base.Update(gameTime, Velocity, Direction);
         }
         private void UpdateMovement(KeyboardState currentKeyboardState, GameTime gameTime)
@@ -44,38 +43,90 @@ namespace DangerousGame
             Velocity = Vector2.Zero;
             Direction = Vector2.Zero;
   
-            // Left and Right arrow keys
+            // Left Arrow Key
             if (currentKeyboardState.IsKeyDown(Keys.Left))
+            {
+                if (!pressedKeys.Contains(Keys.Left))
+                    pressedKeys.Add(Keys.Left);
+            }
+            else if (pressedKeys.Contains(Keys.Left))
+            {
+                pressedKeys.Remove(Keys.Left);
+            }
+
+            // Right Arrow Key
+            if (currentKeyboardState.IsKeyDown(Keys.Right))
+            {
+                if (!pressedKeys.Contains(Keys.Right))
+                    pressedKeys.Add(Keys.Right);
+            }
+            else if (pressedKeys.Contains(Keys.Right))
+            {
+                pressedKeys.Remove(Keys.Right);
+            }
+
+            // Up Arrow Key
+            if (currentKeyboardState.IsKeyDown(Keys.Up))
+            {
+                if (!pressedKeys.Contains(Keys.Up))
+                    pressedKeys.Add(Keys.Up);
+            }
+            else if (pressedKeys.Contains(Keys.Up))
+            {
+                pressedKeys.Remove(Keys.Up);
+            }
+
+            // Down Arrow Key
+            if (currentKeyboardState.IsKeyDown(Keys.Down))
+            {
+                if (!pressedKeys.Contains(Keys.Down))
+                    pressedKeys.Add(Keys.Down);
+            }
+            else if (pressedKeys.Contains(Keys.Down))
+            {
+                pressedKeys.Remove(Keys.Down);
+            }
+
+            if (pressedKeys.Count > 0)
+                lastPressedKey = pressedKeys[pressedKeys.Count - 1];
+            else
+                lastPressedKey = 0;
+
+            //foreach (Keys key in pressedKeys)
+            //    Console.Out.WriteLine(key);
+
+            Console.Out.WriteLine(pressedKeys.Count);
+
+            if (lastPressedKey == Keys.Left)
             {
                 Velocity.X = Speed;
                 Direction.X = MOVE_LEFT;
                 Play("walkLeft");
             }
-            else if (currentKeyboardState.IsKeyDown(Keys.Right))
+            else if (lastPressedKey == Keys.Right)
             {
                 Velocity.X = Speed;
                 Direction.X = MOVE_RIGHT;
                 Play("walkRight");
             }
-            
-            if
-                (currentKeyboardState.IsKeyDown(Keys.Up))
+            else if (lastPressedKey == Keys.Up)
             {
                 Velocity.Y = Speed;
                 Direction.Y = MOVE_UP;
                 Play("walkUp");
             }
-            else if (currentKeyboardState.IsKeyDown(Keys.Down))
+            else if (lastPressedKey == Keys.Down)
             {
                 Velocity.Y = Speed;
                 Direction.Y = MOVE_DOWN;
                 Play("walkDown");
             }
 
+            // Let the animation stop when the player has no speed.
             if (Velocity.Equals(Vector2.Zero))
                 Stop();
-            
 
+            // Check or the character can walk to his new position. If not, Dont let him walk to there.
             Vector2 newPosition = Position + (Direction * Velocity * (float)gameTime.ElapsedGameTime.TotalSeconds);
             if (!Map.MayWalk(newPosition))
             {
@@ -83,11 +134,7 @@ namespace DangerousGame
                 Direction = Vector2.Zero;
             }
             else
-            {
                 Map.SetCenterPosition(newPosition);
-            }
-                
-
         }
 
         public void setPosition(Vector2 value)
