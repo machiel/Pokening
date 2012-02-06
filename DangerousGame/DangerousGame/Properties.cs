ab5a5129f007905e9520a753a9293c5990b8084f
Machiel Molenaar <machiel06@gmail.com>
2012-02-06 10:28:27 +0100

    All files should be in place now.

---
+++ DangerousGame/DangerousGame/Properties.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DangerousGame
+{
+    class Properties
+    {
+        public const int WindowWidth = 800;
+        public const int WindowHeight = 600;
+
+        public const int TileWidth = 50;
+        public const int TileHeight = 50;
+
+        public const int MainCharacterWidth = 32;
+        public const int MainCharacterHeight = 46;
+    }
+}
