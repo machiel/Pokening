aaf0e2befe1f5c830753046328a7a5364cd3db21
Machiel Molenaar <machiel06@gmail.com>
2012-02-09 14:27:04 +0100

    Forgot to commit Player.cs

---
+++ DangerousGame/DangerousGame/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DangerousGame
+{
+    class Player
+    {
+
+        private List<Monster> Monsters;
+
+        public Player(List<Monster> availableMonsters)
+        {
+
+            Monster p = availableMonsters[3];
+            p.Reset(5);
+
+            Monsters = new List<Monster>();
+            Monsters.Add(p);
+        }
+
+        public List<Monster> GetMonsters()
+        {
+            return Monsters;
+        }
+    }
+}
